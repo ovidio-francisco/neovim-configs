1b52156fb9e015042d6ab629b95a98502122c04b
Ovídio Francisco <ovidiojf@gmail.com>
2023-10-31 22:16:58 -0300

    Found nice colors for cursor modes

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -7,13 +7,14 @@ vim.api.nvim_set_hl(0, 'Special', { fg = "#eb6f92" })
 vim.api.nvim_set_hl(0, 'ModeMsg', { bold=true, fg = "white" })
 
 
--- vim.api.nvim_set_hl(0, 'nCursor', { bg = "#f6c177" })
-vim.api.nvim_set_hl(0, 'nCursor', { bg = "DarkCyan" })
+vim.api.nvim_set_hl(0, 'nCursor', { bg = "#f6c177" })
+-- vim.api.nvim_set_hl(0, 'nCursor', { bg = "DarkCyan" })
 -- vim.api.nvim_set_hl(0, 'nCursor', { bg = "White" })
 
 -- vim.api.nvim_set_hl(0, 'iCursor', { bg = "Orange" })
 -- vim.api.nvim_set_hl(0, 'iCursor', { bg = "#f6c177" })
-vim.api.nvim_set_hl(0, 'iCursor', { bg = "White" })
+-- vim.api.nvim_set_hl(0, 'iCursor', { bg = "White" })
+vim.api.nvim_set_hl(0, 'iCursor', { bg = "#E66100" })
 
 
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
